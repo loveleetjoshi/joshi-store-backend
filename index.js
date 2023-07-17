7288650161be8637c86779982f6b4b9d084d7a13
entebyt <45286729+entebyt@users.noreply.github.com>
2023-07-17 21:30:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const paymentRoutes = require("./src/routes/paymentRoutes");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://joshistore.netlify.app"],
+    origin: ["http://localhost:3000"],
   })
 );
 app.use(bodyParser.json()); // Parse JSON bodies
